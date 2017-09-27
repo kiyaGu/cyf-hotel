65fafa1ebce48c963e0dd8fa778da4c38c0db3ec
KiyaG <hamilee.kiya@gmail.com>
2017-09-27 15:30:42 +0100

    completed exercise 1 -4

--- server/api.js
+++ server/api.js
@@ -1,116 +1,185 @@
 const express = require('express');
 const router = express.Router();
 const filename = './database/database.sqlite';
-const sqlite3    = require( 'sqlite3' ).verbose();
+const sqlite3 = require('sqlite3').verbose();
 
 // open the database
 let db = new sqlite3.Database(filename);
-  
+
 router.get('/customers', function(req, res) {
 
-  res.status(200).json({
-    customers: [{
-      id: 2,
-      title: 'Mr',
-      firstname: 'Laurie',
-      surname: 'Ainley',
-      email: 'laurie@ainley.com'
-    }
-  ]});
+    // res.status(200).json({
+    //     customers: [{
+    //         id: 2,
+    //         title: 'Mr',
+    //         firstname: 'Laurie',
+    //         surname: 'Ainley',
+    //         email: 'laurie@ainley.com'
+    //     }]
+    // });
+    // TODO comment out response above and uncomment the below
+    db.serialize(function() {
 
-  // TODO comment out response above and uncomment the below
-  /*db.serialize(function() {
+        var sql = 'select * from customers';
 
-    var sql = 'select * from customers';
-
-    db.all(sql, [],(err, rows ) => {
-      res.status(200).json({
-        customers: rows
-      });  
+        db.all(sql, [], (err, rows) => {
+            res.status(200).json({
+                customers: rows
+            });
+        });
     });
-  }); */
 
 });
 
 router.post('/customers', function(req, res) {
-  // TODO read req.body.customer and insert into DB
-  res.status(200).json( req.body.customer );
-});
+    // TODO read req.body.customer and insert into DB
+    const {
+        title,
+        firstname,
+        surname,
+        email
+    } = req.body.customer;
+    let sql = `INSERT INTO customers (title,firstname,surname,email) 
+    VALUES (?,?,?,?)`;
+    db.run(sql, [`${title}`, `${firstname}`, `${surname}`, `${email}`], function(err) {
+        if (err) {
+            console.error(err)
+        } else {
 
+            res.status(200).json({ message: "Reccord inserted successfuly" });
+        }
+
+    });
+
+
+});
 router.get('/room-types', function(req, res) {
-  // TODO return DB row here
-  res.status(200).json({
-    roomtypes: [{
-      id: 2,
-      name: 'premium',
-      standard_price: 60,
-      current_price: 50
-    }
-  ]});
+    // TODO return DB row here
+    db.serialize(function() {
+        var sql = 'select * from room_types ORDER BY original_price ASC';
+        db.all(sql, [], (err, rows) => {
+            let temp = [];
+            rows.forEach((element) => {
+                let price;
+                if (element.original_price === element.current_price) {
+                    price = '£' + element.original_price / 100;
+                } else {
+                    price = `<span style = 'text-decoration:line-through'>£${element.original_price/ 100}</span> £${element.current_price/ 100}`
+                }
+                temp.push({
+                    id: element.id,
+                    name: element.type_name,
+                    price: price
+                });
+            })
+            res.status(200).json({
+                roomtypes: temp
+            });
+        });
+    });
+
+    //current_price does not equal the standard_price
+    //text-decoration: line-through
+    // res.status(200).json({
+    //     roomtypes: [{
+    //         id: 2,
+    //         name: 'premium',
+    //         standard_price: 60,
+    //         current_price: 50
+    //     }]
+    // });
 });
 
 router.put('/discount', function(req, res) {
-  // TODO read roomId from req.query.id and update discount
-  res.status(200).json({
-    id: req.query.id,
-    discount: req.body.discount
-  });
+    // TODO read roomId from req.query.id and update discount
+
+    let sql = 'select * from room_types where id = ?';
+    //req.query.id => to get the id that is passed as part of the url
+    db.all(sql, [req.query.id], (err, currentPrice) => {
+        console.log(currentPrice)
+            //assumption: the discount is given in % as 10 means 10% and the discount is done on the 
+            //the current price if the current and the original price are equal it is a new discount on the original price else
+            // it is it is a further reduction on the current_price
+        let newPrice,
+            current_price = currentPrice[0].current_price,
+            discountRate = req.body.discount;
+
+        newPrice = current_price - (current_price * (discountRate / 100));
+        let updateSql = `UPDATE room_types
+                        SET  current_price = ?
+                        WHERE id = ?`;
+        db.run(updateSql, [`${newPrice }`, `${req.query.id }`], (err) => {
+                if (err) {
+                    console.error(err)
+                } else {
+
+                    res.status(200).json({ message: "Reccord updated successfuly" });
+                }
+
+            })
+            // res.status(200).json(rows);
+            // res.status(200).json({
+            //     id: req.query.id,
+            //     discount: req.body.discount
+            // });
+    });
+
 });
 
 router.post('/reservations', function(req, res) {
-  // TODO read req.body.reservation, look up price by room id and insert reservation into DB
-  res.status(200).json(req.body.reservation);
+    // TODO read req.body.reservation, look up price by room id and insert reservation into DB
+    res.status(200).json(req.body.reservation);
 });
 
 router.get('/reservations', function(req, res) {
-  // TODO read req.query.name or req.query.id to look up reservations and return
-  res.status(200).json({
-    reservations:[{
-      title: 'Mr',
-      firstname: 'Laurie',
-      surname: 'Ainley',
-      email: 'laurie@ainley.com',
-      roomId: 1,
-      checkInDate: '2017-10-10',
-      checkOutDate: '2017-10-17'
-    }]
-  });
+    // TODO read req.query.name or req.query.id to look up reservations and return
+    res.status(200).json({
+        reservations: [{
+            title: 'Mr',
+            firstname: 'Laurie',
+            surname: 'Ainley',
+            email: 'laurie@ainley.com',
+            roomId: 1,
+            checkInDate: '2017-10-10',
+            checkOutDate: '2017-10-17'
+        }]
+    });
 });
 
 router.get('/reservations/date-from/:dateFrom', function(req, res) {
-  // TODO read req.params.dateFrom to look up reservations and return
-  res.status(200).json({
-    reservations:[{
-      title: 'Mr',
-      firstname: 'Laurie',
-      surname: 'Ainley',
-      email: 'laurie@ainley.com',
-      roomId: 1,
-      checkInDate: '2017-10-10',
-      checkOutDate: '2017-10-17'
-    }, {
-      title: 'Miss',
-      firstname: 'Someone',
-      surname: 'Else',
-      email: 'someone@else.com',
-      roomId: 2,
-      checkInDate: '2017-11-12',
-      checkOutDate: '2017-11-15'
-    }]
-  });
+    // TODO read req.params.dateFrom to look up reservations and return
+    res.status(200).json({
+        reservations: [{
+            title: 'Mr',
+            firstname: 'Laurie',
+            surname: 'Ainley',
+            email: 'laurie@ainley.com',
+            roomId: 1,
+            checkInDate: '2017-10-10',
+            checkOutDate: '2017-10-17'
+        }, {
+            title: 'Miss',
+            firstname: 'Someone',
+            surname: 'Else',
+            email: 'someone@else.com',
+            roomId: 2,
+            checkInDate: '2017-11-12',
+            checkOutDate: '2017-11-15'
+        }]
+    });
 });
 
 router.put('/invoice', function(req, res) {
-  // TODO read req.query.reservationId and req.body.invoice and insert into DB
-  res.status(200).json({
-    reservationId: req.query.reservationId,
-    invoice: req.body.invoice
-  });
+    // TODO read req.query.reservationId and req.body.invoice and insert into DB
+    res.status(200).json({
+        reservationId: req.query.reservationId,
+        invoice: req.body.invoice
+    });
 });
 
 router.post('/reviews', function(req, res) {
-  // TODO read req.body.review
-  res.status(200).json(req.body);
+    // TODO read req.body.review
+    res.status(200).json(req.body);
 });
 
 module.exports = router;
